6ce18112c8fbd753ff9ca277f19ef347ca1f15b4
João Dias <joaovitorelf@gmail.com>
2023-06-13 13:09:44 -0300

    Decisão IF

---
+++ Decisoes/decisao_if.py
@@ -0,0 +1,8 @@
+nome = input("Digite um nome: ")
+idade = int(input("Digite uma idade: "))
+
+# Estrutura de decisão IF
+if idade >= 65:
+    print("O paciente " + nome + " possui atendimento prioritário!")
+else:
+    print("O paciente " + nome + " não possui atendimento prioritário!")
