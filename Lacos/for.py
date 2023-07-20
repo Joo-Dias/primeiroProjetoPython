24a35350e34cb51d6b6d4e7fc72de6c7e3a2a422
João Dias <joaovitorelf@gmail.com>
2023-07-19 23:44:29 -0300

    Laço FOR

---
+++ Lacos/for.py
@@ -0,0 +1,8 @@
+# Laço de repetição FOR
+
+numero = 0
+
+# I = Contador | (inicio, fim, incremento)
+# O laço irá repetir até a variável 'i' chegar a 11
+for i in range(0, 11, 1):
+    print(numero + i)
