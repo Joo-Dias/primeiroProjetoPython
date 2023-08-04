f730450cd927c1f6f0517144ff48d4cb9cfc2bbb
João Dias <joaovitorelf@gmail.com>
2023-08-04 20:11:21 -0300

    Manipulando Strings

---
+++ Manipulacao_Arquivos/textos.py
@@ -0,0 +1,14 @@
+texto = "Leia e Han solo"
+#        0123456789.....
+
+# Pegando a posicao dos caracteres na variavel texto
+print(texto[0:4])
+print(texto[0:4:2])
+print(texto[7:])
+
+# Podemos usar valores negativos para manipular
+print(texto[-8:])
+
+# Podemos inverter a string tambem
+print(texto[::-1])
+
